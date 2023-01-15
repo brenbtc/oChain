70c88d04d3df88f06fc555d2a9399820f3551896
brenbtc <yanohb@yahoo.com>
2023-01-14 21:06:55 -0700

    Commiting before starting

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router()
 const db = require('../models');
-// const User = db.User;
 const jwt = require('jwt-simple');
 const config = require('../config/config')
 
