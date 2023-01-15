8b9ef398feda18dfb76ea107ce890f9a916d55ff
brenbtc <yanohb@yahoo.com>
2023-01-14 21:16:23 -0700

    Login Psuedo

--- frontend/src/components/Login/login.js
+++ frontend/src/components/Login/login.js
@@ -2,10 +2,25 @@ import './login.css'
 
 function Login() {
 
+    
 
     return (
         <div>
-        
+        <h1>Login</h1>
+        <form>
+            <label htmlFor='username'>Username: </label>
+            <input 
+            type='text'
+            name='username'
+            />
+            <label htmlFor='password'>Password: </label>
+            <input
+            type='text'
+            name='username'
+            />
+
+            <button type='submit'>Login</button>
+        </form>
         </div>
     )
 }
